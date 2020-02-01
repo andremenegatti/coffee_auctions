ae46317631ecf028a75455fbbf83d9f5c2f5af4a
André Luís Menegatti <andre.menegatti@hotmail.com>
2020-02-01 13:00:27 -0300

    Ajustes DD win_bid

--- dd/calculando_aumento_gastos.R
+++ dd/calculando_aumento_gastos.R
@@ -1,23 +1,41 @@
+library(tidyverse)
 
+# Definindo variáveis com coeficientes estimados:
+coef_sp <- 114
+coef_brasil <- 114
 
-df_gasto_pos_3s_full <- data_list$cnet_cafe %>%
+# Abrindo bases ---------------------------------------------------------------
+data_list <- 
+  map(.x = str_c('data/', c('bec', 'cnet', 'cnet_sp'), '_cafe_dd.rds'),
+      .f = ~ readRDS(.x) %>%
+        # Removendo outliers: 2.5% de cada lado # <<<<
+        PregoesBR::trim_df('win_bid_kg', perc = 2.5) %>%
+        # Coercing to factor to avoid warnings when joining dataframes
+        filter(kg_por_unid != 0.25) # <<<<
+  )  %>%
+  # Dando nomes aos DFs
+  set_names(c('bec', 'cnet', 'cnet_sp'))
+
+# Brasil ----------------------------------------------------------------------
+df_gasto_pos_3s_brasil <- data_list$cnet %>%
   filter(abertura_lances >= data_3s) %>%
-  select(abertura_lances, inicio_ano, win_bid, quantidade, kg_por_unid) %>%
-  mutate(preco_total = win_bid * kg_por_unid * quantidade)
+  select(abertura_lances, inicio_ano, win_bid_kg, quantidade, kg_por_unid) %>%
+  mutate(preco_total = win_bid_kg * kg_por_unid * quantidade)
 
 # Gasto total apos regra 3s (milhoes de reais)
-gasto_pos_3s_full <- sum(df_gasto_pos_3s_full$preco_total) / 1e+6
+gasto_pos_3s_brasil <- sum(df_gasto_pos_3s_brasil$preco_total) / 1e+6
 
-gasto_pos_3s_full - (gasto_pos_3s_full * 100 / 114)
+# Gasto adicional causado pela regra dos 3s
+gasto_pos_3s_brasil - (gasto_pos_3s_brasil * 100 / coef_brasil)
 
-
-
-df_gasto_pos_3s_sp <- data_list$cnet_cafe_sp %>%
+# SP --------------------------------------------------------------------------
+df_gasto_pos_3s_sp <- data_list$cnet_sp %>%
   filter(abertura_lances >= data_3s) %>%
-  select(abertura_lances, inicio_ano, win_bid, quantidade, kg_por_unid) %>%
-  mutate(preco_total = win_bid * kg_por_unid * quantidade)
+  select(abertura_lances, inicio_ano, win_bid_kg, quantidade, kg_por_unid) %>%
+  mutate(preco_total = win_bid_kg * kg_por_unid * quantidade)
 
 # Gasto total apos regra 3s (milhoes de reais)
 gasto_pos_3s_sp <- sum(df_gasto_pos_3s_sp$preco_total) / 1e+6
 
-gasto_pos_3s_sp - (gasto_pos_3s_sp * 100 / 114)
+# Gasto adicional causado pela regra dos 3s
+gasto_pos_3s_sp - (gasto_pos_3s_sp * 100 / coef_sp)
--- dd/dd_win_bid.R
+++ dd/dd_win_bid.R
@@ -6,19 +6,19 @@ library(lfe)
 # Principais
 bec_cafe <- readRDS('data/bec_cafe_dd.rds')
 cnet_cafe <- readRDS('data/cnet_cafe_dd.rds')
-cnet_cafe_sp <- readRDS('data/cnet_cafe_sp_dd.rds')
+cnet_cafe_sp <- readRDS('data/cnet_sp_cafe_dd.rds')
 
-# Importando DFs com UASGs selecionadas
-selected_uasgs_list <- list(
-  readRDS('data/bec_selected_uasgs_soft_trim.rds'),
-  readRDS('data/cnet_selected_uasgs_soft_trim.rds'),
-  readRDS('data/cnet_sp_selected_uasgs_soft_trim.rds')
-  ) %>%
+# Importando DFs com UASGs selecionadas em uma lista
+selected_uasgs_names <- str_c(c('bec', 'cnet', 'cnet_sp'), '_selected_uasgs')
+
+selected_uasgs_list <- 
+  map(.x = str_c('data/', selected_uasgs_names, '_soft_trim.rds'),
+      .f = readRDS) %>%
   # Selecionando apenas colunas desejadas
   map(.f = ~ select(.x, id_item,unidade_compradora_lasso = lasso) %>%
         mutate(unidade_compradora_lasso = 
                  as.character(unidade_compradora_lasso))) %>% 
-  set_names('bec_selected_uasgs', 'cnet_selected_uasgs', 'cnet_sp_selected_uasgs')
+  set_names(selected_uasgs_names)
 
 # Data wrangling --------------------------------------------------------------
 data_list <- list(bec_cafe, cnet_cafe, cnet_cafe_sp) %>%
@@ -37,9 +37,9 @@ data_list <- list(bec_cafe, cnet_cafe, cnet_cafe_sp) %>%
                ) %>%
         # Coercing to factor to avoid warnings when joining dataframes
         mutate_if(is.factor, as.character) %>%
-        filter(kg_por_unid != 0.25)
+        filter(kg_por_unid != 0.25) # <<<<
       ) %>%
-  # Incluindo coluna com uasgs selecionadas
+  # Incluindo coluna com unidades compradoras selecionadas
   map2(
     .y = selected_uasgs_list,
     .f = ~ left_join(.x, .y, by = 'id_item')
@@ -47,7 +47,7 @@ data_list <- list(bec_cafe, cnet_cafe, cnet_cafe_sp) %>%
   # Dando nomes aos DFs
   set_names(c('bec_cafe', 'cnet_cafe', 'cnet_cafe_sp'))
 
-# Montando bases DD -----------------------------------------------------------
+# Montando bases DD em uma lista ----------------------------------------------
 dd_data_list <- list(data_list$cnet_cafe, data_list$cnet_cafe_sp) %>%
   map(.f = ~ bind_rows(.x, data_list$bec_cafe) %>%
         PregoesBR::build_dd_df()
@@ -59,107 +59,71 @@ dd_data_list <- list(data_list$cnet_cafe, data_list$cnet_cafe_sp) %>%
 #   select(-inicio_ano, -inicio_bimestre, -inicio_mes, -inicio_semana) %>%
 #   haven::write_dta('sp_cafe_dd.dta')
 
-# DD --------------------------------------------------------------------------
-attach(dd_data_list$sp_cafe_dd)
-detach(dd_data_list$sp_cafe_dd)
+# DD SP -----------------------------------------------------------------------
+form <- 'log_win_bid ~ comprasnet + treat1 + treat2'
 
-# Log
-felm1_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 | bimestre)
-felm2_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 | bimestre + unidade_compradora_lasso)
-felm3_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 | bimestre + unidade_compradora_lasso + municipio)
-felm4_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 | bimestre + unidade_compradora_lasso + municipio + marca_vencedor_principais)
-felm5_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade | bimestre + unidade_compradora_lasso + municipio + marca_vencedor_principais)
-felm6_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid | bimestre + unidade_compradora_lasso + municipio + marca_vencedor_principais)
-felm7_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + arab_defl | bimestre + unidade_compradora_lasso + municipio + marca_vencedor_principais)
-felm8_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + futuro_defl | bimestre + unidade_compradora_lasso + municipio + marca_vencedor_principais)
-felm9_log <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + futuro_defl + arab_defl | bimestre + unidade_compradora_lasso + municipio + marca_vencedor_principais)
+df_models_sp <- tibble(
+  formula = 
+    c(form,
+      str_c(form, ' | bimestre + unidade_compradora'),
+      str_c(form, ' | bimestre + unidade_compradora + municipio'),
+      str_c(form, ' | bimestre + unidade_compradora + municipio + marca_vencedor_principais'),
+      str_c(form, ' + qualidade                             | bimestre + unidade_compradora + municipio + marca_vencedor_principais'),
+      str_c(form, ' + qualidade + kg_por_unid               | bimestre + unidade_compradora + municipio + marca_vencedor_principais'),
+      str_c(form, ' + qualidade + kg_por_unid + futuro_defl | bimestre + unidade_compradora + municipio + marca_vencedor_principais'),
+      str_c(form, ' + qualidade + kg_por_unid + arab_defl   | bimestre + unidade_compradora + municipio + marca_vencedor_principais'),
+      str_c(form, ' + qualidade + kg_por_unid + arab_defl + comprasnet:trend_bimestre | bimestre + unidade_compradora + municipio + marca_vencedor_principais')),
+  models = map(.x = formula,
+               .f = ~ felm(as.formula(.x),
+                           data = dd_data_list$sp_cafe_dd))) # <<<<
 
-felm_log_trend <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + futuro_defl + arab_defl + comprasnet:trend_bimestre | bimestre + unidade_compradora_lasso + municipio + marca_vencedor_principais)
+stargazer(df_models_sp$models, type = 'text')
 
+# DD Brasil -------------------------------------------------------------------
+df_models_brasil <- tibble(
+  formula = 
+    c(form,
+      str_c(form, ' | bimestre + sigla_uf:bimestre'),
+      str_c(form, ' | bimestre + sigla_uf:bimestre + municipio'),
+      str_c(form, ' | bimestre + sigla_uf:bimestre + municipio + unidade_compradora'),
+      str_c(form, ' + qualidade                             | bimestre + sigla_uf:bimestre + municipio + unidade_compradora'),
+      str_c(form, ' + qualidade + kg_por_unid               | bimestre + sigla_uf:bimestre + municipio + unidade_compradora'),
+      str_c(form, ' + qualidade + kg_por_unid + futuro_defl | bimestre + sigla_uf:bimestre + municipio + unidade_compradora'),
+      str_c(form, ' + qualidade + kg_por_unid + arab_defl   | bimestre + sigla_uf:bimestre + municipio + unidade_compradora'),
+      str_c(form, ' + qualidade + kg_por_unid + arab_defl + comprasnet:trend_bimestre | bimestre + sigla_uf:bimestre + municipio + unidade_compradora')),
+  models = map(.x = formula,
+               .f = ~ felm(as.formula(.x),
+                           data = dd_data_list$full_cafe_dd))) # <<<<
 
-stargazer(felm1_log,
-          felm2_log,
-          felm3_log,
-          felm4_log,
-          felm5_log,
-          felm6_log,
-          felm7_log,
-          felm8_log,
-          felm9_log,
-          felm_log_trend,
-          type = 'text')
+stargazer(df_models_brasil$models, type = 'text')
 
+# Robust Standard Errors ------------------------------------------------------
+lm_sp <- lm(log_win_bid ~ comprasnet + treat1 + treat2 + arab_defl + 
+              kg_por_unid + qualidade + bimestre + municipio + 
+              unidade_compradora + marca_vencedor_principais,
+            data = dd_data_list$sp_cafe_dd)
 
-# UF e UF x Bimestre
-detach(dd_data_list$sp_cafe_dd)
-attach(dd_data_list$full_cafe_dd)
-
-felm_log_full1  <- felm(log_win_bid ~ comprasnet + treat1 + treat2 | bimestre)
-felm_log_full2  <- felm(log_win_bid ~ comprasnet + treat1 + treat2 | bimestre + sigla_uf:bimestre)
-felm_log_full3  <- felm(log_win_bid ~ comprasnet + treat1 + treat2 | bimestre + sigla_uf:bimestre + municipio)
-felm_log_full4  <- felm(log_win_bid ~ comprasnet + treat1 + treat2 | bimestre + sigla_uf:bimestre + municipio + unidade_compradora_lasso)
-felm_log_full5  <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade | bimestre + sigla_uf:bimestre + municipio + unidade_compradora_lasso)
-felm_log_full6  <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid | bimestre  + sigla_uf:bimestre + municipio + unidade_compradora_lasso)
-felm_log_full7  <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + futuro_defl | bimestre  + sigla_uf:bimestre + municipio + unidade_compradora_lasso)
-felm_log_full8 <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + futuro_defl + arab_defl | bimestre + sigla_uf:bimestre + municipio + unidade_compradora_lasso)
-felm_log_full9 <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + futuro_defl + arab_defl | bimestre + sigla_uf:bimestre + municipio + unidade_compradora_lasso + marca_vencedor_principais)
-
-felm_log_full_trend <- felm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + futuro_defl + arab_defl + comprasnet:trend_bimestre | bimestre  + sigla_uf:bimestre + municipio + unidade_compradora_lasso)
-
-stargazer(felm_log_full1,
-          felm_log_full2,
-          felm_log_full3,
-          felm_log_full4,
-          felm_log_full5,
-          felm_log_full6,
-          felm_log_full7,
-          felm_log_full8,
-          felm_log_full9,
-          felm_log_full_trend,
-          type = 'text')
-
-
-# Robust
-lm_base <- lm(win_bid ~ comprasnet + treat1 + treat2 + futuro_defl + arab_defl + kg_por_unid + qualidade + bimestre + municipio + unidade_compradora + marca_vencedor_principais)
-lm_log <- lm(log_win_bid ~ comprasnet + treat1 + treat2 + futuro_defl + arab_defl + kg_por_unid + qualidade + bimestre + municipio + unidade_compradora + marca_vencedor_principais)
-lm_log_uf <- lm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade + kg_por_unid + futuro_defl + arab_defl + bimestre + sigla_uf:bimestre + municipio + unidade_compradora,
+lm_brasil <- lm(log_win_bid ~ comprasnet + treat1 + treat2 + qualidade 
+                + kg_por_unid + arab_defl + bimestre + sigla_uf:bimestre
+                + municipio + unidade_compradora,
                 data = dd_data_list$full_cafe_dd)
 
+df_std_sp <- PregoesBR::get_robust_std_errors(lm_sp, HC = 'HC1')
+df_std <- PregoesBR::get_robust_std_errors(lm_brasil, HC = 'HC1')
 
-get_robust_std_errors(lm_base, HC = 'HC1')
+# Tendencias à la Chimeli-Soares: efeito do tratamento ao longo dos meses -----
+dd_treat_trends_sp <- 
+  felm(log_win_bid ~ comprasnet + treat1 + treat2 + arab_defl + qualidade + 
+         treat1_trend_bimestre + treat2_trend_bimestre | 
+       bimestre + unidade_compradora + municipio + marca_vencedor_principais,
+     data = dd_data_list$sp_cafe_dd) # <<<<
 
-get_robust_std_errors(lm_log, HC = 'HC1')$p_value[3:4]
-df_std <- get_robust_std_errors(lm_log_uf, HC = 'HC1')
+dd_treat_trends_brasil <- 
+  felm(log_win_bid ~ comprasnet + treat1 + treat2 + arab_defl + qualidade + 
+         kg_por_unid + treat1_trend_bimestre + treat2_trend_bimestre | 
+         bimestre + sigla_uf:bimestre + unidade_compradora + municipio,
+       data = dd_data_list$full_cafe_dd) # <<<<
 
-
-# Residuals
-felm2_res <- felm(res_EF_uasgs ~ comprasnet + treat1 + treat2 | mes)
-felm3_res <- felm(res_EF_uasgs ~ comprasnet + treat1 + treat2 | mes + marca_vencedor_principais)
-felm4_res <- felm(res_EF_uasgs ~ comprasnet + treat1 + treat2 + futuro_defl + arab_rob_defl | mes + marca_vencedor_principais)
-felm5_res <- felm(res_EF_uasgs ~ comprasnet + treat1 + treat2 + futuro_defl + arab_rob_defl | mes + marca_vencedor_principais | 0 | comprasnet)
-felm6_res <- felm(res_EF_uasgs ~ comprasnet + treat1 + treat2 + futuro_defl + arab_rob_defl | mes + marca_vencedor_principais + municipio | 0 | comprasnet)
-
-
-# Teste de robustez: tendencias especificas para cada grupo
-dd_group_trend <- lm(
-  win_bid ~ comprasnet + mes + treat1 + treat2 + unidade_compradora
-  + marca_vencedor_principais + marca_vencedor_principais:comprasnet
-  + futuro_defl + arab_rob_defl + group_trend
-  )
-
-# Tendencias a la Ariaster Chimeli: efeito do tratamento ao longo dos meses
-dd_treat_trends <- lm(
-  formula =
-    win_bid ~ comprasnet + mes + treat1 + treat2 + unidade_compradora
-  + marca_vencedor_principais + marca_vencedor_principais:comprasnet
-  + futuro_defl + arab_rob_defl
-  + treat_trend1 + treat_trend2
-  )
-
-stargazer(dd_group_trend,
-          dd_treat_trends,
-          omit = c("bimestre", "semana", "mes", "unidade_", "marca_", "comprasnet:"),
-          omit.stat = c("ser", "adj.rsq", "f"),
-          decimal.mark = ",",
-          digit.separator = ".",
+stargazer(dd_treat_trends_sp,
+          dd_treat_trends_brasil,
           type = 'text')
